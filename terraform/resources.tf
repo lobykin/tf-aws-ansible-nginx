74ca253f1ebc191993301ac756174484f65702dc
lobykin <72708763+lobykin@users.noreply.github.com>
2020-10-14 01:36:54 +0200

    find

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -94,7 +94,7 @@ resource "aws_instance" "nginx-instance" {
 	    echo "[nginx-instance]" | tee -a nginx-instance.ini;
 	    echo "${aws_instance.nginx-instance[count.index].public_ip} ansible_user=${var.ansible_user} ansible_ssh_private_key_file=${var.private_key}" | tee -a nginx-instance.ini;
       export ANSIBLE_HOST_KEY_CHECKING=False;
-	    ansible-playbook -u ${var.ansible_user} --private-key ${var.private_key} -i nginx-instance.ini ${abspath(path.playbooks)}/nginx_install.yaml
+	    ansible-playbook -u ${var.ansible_user} --private-key ${var.private_key} -i nginx-instance.ini ${abspath(path.root)}/playbooks/nginx_install.yaml
     EOT
   }
 
