699da8b3ae4a80adc3d1355f95f783c35f96d554
lobykin <72708763+lobykin@users.noreply.github.com>
2020-10-13 23:50:31 +0200

    Testing S3

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 terraform {
   required_version = ">=0.13.4"
-  #backend "s3" {
-  #  bucket         = "nginx-dynamo-bucket-888"
-  #  key            = "terraform.tfstate"
-  #  region         = "us-east-1"
-  #  dynamodb_table = "nginx-locks"
-  #  encrypt        = true
-  #  }
+  backend "s3" {
+    bucket         = "nginx-dynamo-bucket-888"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "nginx-locks"
+    encrypt        = true
+    }
 }
 
 # Call the seed_module to build Nginx App
